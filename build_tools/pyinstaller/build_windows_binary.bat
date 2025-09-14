b02df70d01ce8f71ddaa7efabaec9fa41f409f95
Viers <krusnik.viers@gmail.com>
2025-09-14 21:44:09 +0400

    Minor fixes for bin-build

--- build_tools/pyinstaller/build_windows_binary.bat
+++
@@ -1,9 +0,0 @@
-:: Change directory to the
-cd %~dp0\..\
-echo Building .exe from %cd%
-
-pyinstaller.exe --clean                 ^
-    --workpath "./build/temp/"          ^
-    --distpath "./build/"               ^
-    build_tools/pyinstaller/binary.spec ^
-&& rmdir /s /q %cd%\build\temp
