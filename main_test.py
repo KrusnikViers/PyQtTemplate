b59e6170feff827d8265ed58d671222b4a6bbacb
Viers <krusnik.viers@gmail.com>
2026-02-19 17:18:28 +0100

    Attempt to run tests without xvfb

--- main_test.py
+++ main_test.py
@@ -8,7 +8,7 @@ from main import Application
 
 def _simulate_normal_close():
     app = Application.get_or_create()
-    app.activeWindow().close()
+    app.main_window.close()
 
 
 def _force_quit():
@@ -20,8 +20,8 @@ class MainApplicationTest(unittest.TestCase):
     def test_main_starts_and_finishes(self):
         # Create an App early for timers to latch on.
         app = Application.get_or_create()
-        QTimer.singleShot(2000, _simulate_normal_close)
-        QTimer.singleShot(3000, _force_quit)
+        QTimer.singleShot(500, _simulate_normal_close)
+        QTimer.singleShot(1000, _force_quit)
 
         return_code = app.exec()
         self.assertEqual(return_code, 0)
